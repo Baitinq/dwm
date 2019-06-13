e3043882e126bf346646e02aa8ac2d9cf798d11d
Manuel Palenzuela <manuelpalenzuelamerino@gmail.com>
2019-06-13 14:33:19 +0200

    Formatting

--- config.h
+++ config.h
@@ -14,8 +14,10 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -28,7 +30,6 @@ static const unsigned int alphas[][3]      = {
 	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
 	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 	[SchemeTitle]= { OPAQUE, baralpha,  borderalpha},
-
 };
 
 /* tagging */
